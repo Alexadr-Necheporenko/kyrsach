87bcd747b55ad6958e7cd9970b1b752be0b97b32
Alexadr-Necheporenko <al.necheporenko@gmail.com>
2024-05-26 01:14:13 +0300

    BD

--- data/config.py
+++ data/config.py
@@ -1,8 +1,8 @@
-BOT_TOKEN = '**********:**************************'
+BOT_TOKEN = '7034269892:AAGQ54_MdeP9DCMY_XrjNR45TmTddNY3ff8'
 
-PROJECT_NAME = 'store-bot-example'
+PROJECT_NAME = 'store-bot-123'
 
-WEBHOOK_HOST = f"https://{PROJECT_NAME}.herokuapp.com"
+WEBHOOK_HOST = f"https://store-bot-123-73ed5fabae1e.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
